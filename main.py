aa4db025e754b72e23d8de37e7ee441b442bb8ea
Petr Tolmanov <petertolm@gmail.com>
2025-07-27 13:31:11 +0100

    Added root head endpoint

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ class CorrRequest(BaseModel):
     end: date
 
 
-@app.get("/")
+@app.get("/", include_in_schema=False)
+@app.head("/", include_in_schema=False)
 async def root():
     return {"Hello": "Correlation"}
 
